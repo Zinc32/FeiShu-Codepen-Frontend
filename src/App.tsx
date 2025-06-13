ea7dc049d83613e153eca90c19454f955abad264
zinc <221250136@smail.nju.edu.cn>
2025-06-13 15:15:33 +0800

    初始化框架代码

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import EditorPage from './pages/EditorPage';
+import styled from '@emotion/styled';
+
+const AppContainer = styled.div`
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <AppContainer>
+        <Routes>
+          <Route path="/" element={<EditorPage />} />
+        </Routes>
+      </AppContainer>
+    </Router>
   );
 }
 
